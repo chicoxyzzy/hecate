2024babd9389bbb79dcc17a3e5e75999e5e36be8
Sergey Rubanov <chi187@gmail.com>
2026-04-24 00:37:38 +0200

    split app shell and shared primitive styles

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { ErrorBoundary } from "./app/ErrorBoundary";
 import "./styles.css";
+import "./app/App.css";
+import "./features/shared/ConsolePrimitives.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
